5dce910180dd94c7b32c7a07c510cf01311f5e13
Ramón Roche <mrpollo@gmail.com>
2015-03-13 16:49:52 -0700

    pypi setup

---
+++ setup.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""`sphinx_3dr_theme` lives on `Github`_.
+
+.. _github: https://www.github.com/3drobotics/sphinx_3dr_theme
+
+"""
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+from sphinx_3dr_theme import __version__
+
+here = path.abspath(path.dirname(__file__))
+
+setup(
+    name='sphinx_3dr_theme',
+    version=__version__,
+    url='https://github.com/3drobotics/sphinx_3dr_theme/',
+    license='MIT',
+    author='3D Robotics',
+    author_email='droneapi@3drobotics.com',
+    description='3D Robotics official Sphinx theme.',
+    long_description=open('README.md').read(),
+    zip_safe=False,
+    packages=['sphinx_3dr_theme'],
+    package_data={'sphinx_3dr_theme': [
+        'theme.conf',
+        '*.html',
+        'static/css/*.css',
+        'static/js/*.js',
+        'static/font/*.*'
+    ]},
+    include_package_data=True,
+    install_requires=open('requirements.txt').read().splitlines(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: BSD License',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+        'Topic :: Documentation',
+        'Topic :: Software Development :: Documentation',
+    ],
+)
+
--- sphinx_3dr_theme/__init__.py
+++ sphinx_3dr_theme/__init__.py
@@ -1,5 +1,10 @@
 import os
 
+VERSION = (0, 0, 1)
+
+__version__ = ".".join(str(v) for v in VERSION)
+__version_full__ = __version__
+
 def get_html_theme_path():
     """Return list of HTML theme paths."""
     cur_dir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
